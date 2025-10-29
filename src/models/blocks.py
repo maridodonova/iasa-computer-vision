0d9b95179441c6237a83f454b407ffbea5db5fc2
Mariia Dodonova <maridodonova@gmail.com>
2025-10-29 16:04:07 +0200

    refactor SEBlock

--- src/models/blocks.py
+++ src/models/blocks.py
@@ -43,26 +43,38 @@ class ResBlock(nn.Module):
 class SEBlock(nn.Module):
     def __init__(
             self,
-            kernel_size: tuple,
-            num_channels: int,
-            reducted_dim: int
+            in_channels: int,
+            reduction: int
     ) -> None:
         super().__init__()
-        self.squeeze = nn.AvgPool2d(kernel_size)
 
-        self.excitation = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(num_channels, reducted_dim),
+        assert reduction >= 1, "reduction must be >= 1"
+        assert in_channels >= 1, "in_channels must be >= 1"
+        assert reduction <= in_channels, "reduction must be <= in_channels"
+
+        self.in_channels = in_channels
+        self.reduction = reduction
+        self.reduced_channels = self.in_channels // self.reduction
+
+        self.mlp = nn.Sequential(
+            nn.Linear(self.in_channels, self.reduced_channels),
             nn.ReLU(),
-            nn.Linear(reducted_dim, num_channels),
-            nn.Sigmoid(),
-            nn.Unflatten(dim=1, unflattened_size=(num_channels, 1, 1))
+            nn.Linear(self.reduced_channels, self.in_channels)
         )
+        self.unflatten = nn.Unflatten(dim=-1, unflattened_size=(self.in_channels, 1, 1))
+        self.sigmoid = nn.Sigmoid()
     
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        weights = self.squeeze(x)
-        weights = self.excitation(weights)
-        scaled = x * weights
+        # Squeeze
+        spatial_dims = (-2, -1)
+        squeezed = torch.mean(x, dim=spatial_dims)
+
+        # Excitation
+        weights = self.mlp(squeezed)
+        weights = self.unflatten(weights)
+        weights = self.sigmoid(weights)
+        scaled = weights * x
+
         return scaled
 
 
